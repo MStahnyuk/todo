22fa4bac7764981a19877e1f5210a7e17397052f
Maks Stahnyuk <maks.stahnyuk@gmail.com>
2019-09-14 19:17:35 +0300

    add doubleCkick

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -12,11 +12,11 @@ class TodoListItem extends React.Component {
             <li className={"todoListItem checked-" + isCompleted + " edit-" + edit }>
                 <input type="checkbox" id={'checkbox-' + id} checked={isCompleted}  onChange={() => this.props.changeDone(id)}/>
                 <label htmlFor={'checkbox-' + id}><i className="fa fa-check"></i></label>
-                <div className="text" onDoubleClick={() => this.props.changeEdit(id)}>
+                <div className="text" onDoubleClick={() => this.props.changeEdit(id, 'div')}>
                     {value}
                     {/* <input type="text" value={value}/> */}
                 </div>        
-                <input className="editInput" id={"editInput-id-" + id}  value={value} onChange={(event) => this.props.editText(event, id)} type="text" onBlur={() => this.props.changeEdit(id)}/>
+                <input className="editInput" id={"editInput-id-" + id}  value={value} onChange={(event) => this.props.editText(event, id)} type="text" onBlur={() => this.props.changeEdit(id, 'input')}/>
                 <button className="delete" onClick={() => this.props.deleteItem(id)}><i className="fa fa-times"></i></button>
             </li>
         )
@@ -36,10 +36,12 @@ function mapDispatchToProps(dispatch) {
         deleteItem: id => dispatch(deleteItem(id)),
         changeContenteditable: id => dispatch(changeContenteditable(id)),
         editText: (event, id) => dispatch(edit({ 'value': event.target.value, id: id})),
-        changeEdit: (id) => {            
+        changeEdit: (id, elem) => {            
             dispatch(changeEdit(id));
-            let input = document.getElementById('editInput-id-' + id);
-            input.focus();
+            if(elem === 'div') {
+                let input = document.getElementById('editInput-id-' + id);
+                input.focus();
+            }
         },
     }
 }
